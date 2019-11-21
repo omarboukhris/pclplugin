b31f2989eb5df5e213b82ad328312258da1982d6
Julia <julia.coste-marin@etu.unistra.fr>
2019-11-21 11:40:31 +0100

    "Point Generation updated: decide to add points or not"

--- src/sofa/PCLPlugin/PointGeneration.h
+++ src/sofa/PCLPlugin/PointGeneration.h
@@ -13,6 +13,8 @@ using core::objectmodel::Data;
 
 class PointGeneration : public core::objectmodel::BaseObject {
 public:
+    typedef core::objectmodel::BaseObject Inherit;
+
     SOFA_CLASS(PointGeneration,core::objectmodel::BaseObject);
 
     Data<collisionAlgorithm::DetectionOutput> d_pointsInstrument;
@@ -20,15 +22,25 @@ public:
     Data<helper::vector<defaulttype::Vector3>> d_restPoints;
     Data<helper::vector<defaulttype::Vector3>> d_deformedPoints;
 
+    core::objectmodel::DataCallback c_detectionCallback;
+
 
     core::objectmodel::SingleLink<PointGeneration,sofa::core::topology::BaseMeshTopology,BaseLink::FLAG_STRONGLINK|BaseLink::FLAG_STOREPATH> l_topology;
 
     PointGeneration();
     virtual ~PointGeneration();
     virtual void init();
+    void draw(const core::visual::VisualParams *);
+
+
+    void checkPoints();
 
     /// Function that checks the distance to other points and add point
-    void addPoint();
+    void addPoint(collisionAlgorithm::DetectionOutput detectionOutput, unsigned i);
+
+    bool isInTheMesh(collisionAlgorithm::DetectionOutput::PairDetection);
+
+    bool isFarEnough(defaulttype::Vector3 position);
 
 };
 
